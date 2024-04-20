85c8574b96e18dbc1e2355f1f5fcd442a3e3edb1
Kate Gerber <kate@gerber.id.au>
2024-04-20 11:14:29 +1000

    implement add and view function

---
+++ extranotes.py
@@ -0,0 +1,7 @@
+#match choice:
+        #case "1":
+           # print("You entered 1.")
+       # case "2":
+            #print("You entered 2.")
+        #case _:
+            #print("You entered a jargon value.")
